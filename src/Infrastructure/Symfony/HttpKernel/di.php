c7d3e4e93dc9fd545d7f6ce055df3f47332a64e5
Венделев Артём <artiom@vendelev.ru>
2024-04-16 21:12:11 +0300

    Удаление неиспользуемого подписчика

--- src/Infrastructure/Symfony/HttpKernel/di.php
+++ src/Infrastructure/Symfony/HttpKernel/di.php
@@ -6,9 +6,8 @@ namespace CarVolunteer\Infrastructure\Symfony\HttpKernel;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $di, ContainerBuilder $containerBuilder): void {
+return static function (ContainerBuilder $containerBuilder): void {
     $containerBuilder->addCompilerPass(new class () implements CompilerPassInterface {
         public function process(ContainerBuilder $container): void
         {
