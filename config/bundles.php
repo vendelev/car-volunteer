f8aa8c6f2d94b5295b1778b6c6e5bca7407af251
Венделев Артём <artiom@vendelev.ru>
2024-05-03 09:25:13 +0300

    Откатил E_USER_DEPRECATED Исключение E_USER_DEPRECATED Добавил группу пакетов doctrine/orm

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
