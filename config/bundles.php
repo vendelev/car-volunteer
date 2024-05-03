eb34e89f223b2a68e3014071acdd49dcd00dcd33
Венделев Артём <artiom@vendelev.ru>
2024-05-03 09:08:36 +0300

    Добавил boshurik/telegram-bot-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    BoShurik\TelegramBotBundle\BoShurikTelegramBotBundle::class => ['all' => true],
 ];
