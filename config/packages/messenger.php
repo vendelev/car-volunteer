3ba414cc40347ec77db04c9f9addcb0ddac20a67
Венделев Артём <artiom@vendelev.ru>
2024-05-06 09:45:15 +0300

    Временно игнорируем ошибки

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 use Symfony\Config\FrameworkConfig;
 
+/* @phpstan-ignore-next-line */
 return static function (FrameworkConfig $framework): void {
-    $messenger = $framework->messenger();
+    $messenger = $framework->messenger(); // @phpstan-ignore-line
     $messenger->transport('sync')->dsn('sync://');
 };
