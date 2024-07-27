17a369f465b65e9870c68084c9e0d2b34d950a4f
Венделев Артём <artiom@vendelev.ru>
2024-07-27 21:31:12 +0300

    Добавил проверку на существование при регистрации

--- src/Module/Telegram/User/Application/UseCase/RegisterUserUseCase.php
+++ src/Module/Telegram/User/Application/UseCase/RegisterUserUseCase.php
@@ -6,17 +6,23 @@ namespace CarVolunteer\Module\Telegram\User\Application\UseCase;
 
 use CarVolunteer\Module\Telegram\Domain\User as UserDto;
 use CarVolunteer\Module\Telegram\User\Entity\User;
+use CarVolunteer\Module\Telegram\User\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 final readonly class RegisterUserUseCase
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private UserRepository $repository
     ) {
     }
 
     public function handle(UserDto $userDto): void
     {
+        if ($this->repository->findOneBy(['id' => $userDto->id])) {
+            return;
+        }
+
         $entity = new User(
             id: $userDto->id,
             username: $userDto->username,
