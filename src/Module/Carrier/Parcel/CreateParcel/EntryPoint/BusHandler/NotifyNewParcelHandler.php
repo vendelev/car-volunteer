29a8b91a6c246aba76e06c6bceb722bbf58b87da
Венделев Артём <artiom@vendelev.ru>
2024-09-17 20:50:25 +0300

    #11 Проверка прав при уведомлении о новой посылке

--- src/Module/Carrier/Parcel/CreateParcel/EntryPoint/BusHandler/NotifyNewParcelHandler.php
+++ src/Module/Carrier/Parcel/CreateParcel/EntryPoint/BusHandler/NotifyNewParcelHandler.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CarVolunteer\Module\Carrier\Parcel\CreateParcel\EntryPoint\BusHandler;
 
+use CarVolunteer\Domain\User\AuthorizeAttribute;
+use CarVolunteer\Domain\User\UserRole;
 use CarVolunteer\Module\Carrier\Parcel\CreateParcel\Infrastructure\Responder\NotifyTelegramResponder;
 use CarVolunteer\Module\Carrier\Parcel\Domain\ParcelCreatedEvent;
 use HardcorePhp\Infrastructure\MessageBusBundle\Mapping\Handler;
@@ -29,8 +31,9 @@ final readonly class NotifyNewParcelHandler
     #[Handler]
     public function handle(ParcelCreatedEvent $event, MessageContext $messageContext): void
     {
-        $users = array_filter($this->roles['Manager'], static fn($id) => $id !== $event->userId);
-        $commands = $this->responder->getCreateParcelMessages($users, $event->parcel);
+        $roles = $messageContext->getAttribute(AuthorizeAttribute::class)->roles ?? [];
+        $users = array_filter($this->roles[UserRole::Manager->name], static fn($id) => $id !== $event->userId);
+        $commands = $this->responder->getCreateParcelMessages($users, $event->parcel, $roles);
         foreach ($commands as $command) {
             $messageContext->dispatch($command);
         }
--- src/Module/Carrier/Parcel/CreateParcel/Infrastructure/Responder/NotifyTelegramResponder.php
+++ src/Module/Carrier/Parcel/CreateParcel/Infrastructure/Responder/NotifyTelegramResponder.php
@@ -5,25 +5,44 @@ declare(strict_types=1);
 namespace CarVolunteer\Module\Carrier\Parcel\CreateParcel\Infrastructure\Responder;
 
 use CarVolunteer\Domain\Telegram\SendMessageCommand;
+use CarVolunteer\Domain\User\UserRole;
+use CarVolunteer\Infrastructure\Telegram\ActionRouteAccess;
+use CarVolunteer\Infrastructure\Telegram\ActionRouteMap;
+use CarVolunteer\Infrastructure\Telegram\ButtonResponder;
+use CarVolunteer\Infrastructure\Telegram\RouteParam;
 use CarVolunteer\Module\Carrier\Parcel\Domain\ParcelPlayLoad;
 use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
 
 final readonly class NotifyTelegramResponder
 {
+    public function __construct(
+        private ButtonResponder $buttonResponder,
+        private ActionRouteAccess $routeAccess,
+    ) {
+    }
+
     /**
      * @param list<string> $users
+     * @param list<UserRole> $roles
      * @return list<SendMessageCommand>
      */
-    public function getCreateParcelMessages(array $users, ParcelPlayLoad $playLoad): array
+    public function getCreateParcelMessages(array $users, ParcelPlayLoad $playLoad, array $roles): array
     {
         $result = [];
+        $actionInfo = $this->routeAccess->get(ActionRouteMap::ParcelView, $roles);
+        if ($actionInfo) {
+            $buttons = new InlineKeyboardMarkup([[
+                $this->buttonResponder->generate($actionInfo, new RouteParam('id', $playLoad->id->toString()))
+            ]]);
+        } else {
+            $buttons = null;
+        }
+
         foreach ($users as $userId) {
             $result[] = new SendMessageCommand(
                 $userId,
                 sprintf('Создан новый заказ-наряд: <b>%s</b>', $playLoad->title),
-                new InlineKeyboardMarkup([
-                    [['text' => 'Посмотреть', 'callback_data' => '/viewParcel?id=' . $playLoad->id]]
-                ])
+                $buttons
             );
         }
 
